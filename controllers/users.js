19adcee33545a7ffebc92b75d8d777d78c7bb6c4
Artem Kireev <al-mysri@yandex.ru>
2022-05-15 22:34:11 +0300

    feat: updateProfile and updateAvatar

--- controllers/users.js
+++ controllers/users.js
@@ -14,8 +14,29 @@ module.exports.getUser = (req, res) => {
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
-
   User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send(`${err.message}: 'Произошла ошибка'`));
+};
+
+module.exports.updateProfile = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about }, {
+    new: true,
+    runValidators: true,
+    upsert: true,
+  })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send(`${err.message}: 'Произошла ошибка'`));
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar }, {
+    new: true,
+    runValidators: true,
+    upsert: true,
+  })
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(500).send(`${err.message}: 'Произошла ошибка'`));
 };
