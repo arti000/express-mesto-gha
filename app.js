4be2c2d260b1733b89d1430681fc3941b550d817
Artem Kireev <al-mysri@yandex.ru>
2022-05-19 11:37:50 +0300

    @@

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const bodyParser = require('body-parser');
-
 const userRoutes = require('./routes/users');
 const cardsRoutes = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(bodyParser.json({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true,
+}));
 
 app.use((req, res, next) => {
   req.user = {
