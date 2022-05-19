44c526b420f7cd078ef184fffa4fdff72abbb16e
Artem Kireev <al-mysri@yandex.ru>
2022-05-19 20:40:51 +0300

    fix: delete property upsert

--- controllers/users.js
+++ controllers/users.js
@@ -56,7 +56,6 @@ const updateProfile = (req, res) => {
   User.findByIdAndUpdate(req.user._id, { name, about }, {
     new: true,
     runValidators: true,
-    upsert: true,
   })
     .then((user) => res.status(200).send(user))
     .catch((err) => {
