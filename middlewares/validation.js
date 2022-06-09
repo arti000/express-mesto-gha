4389ee8bda1681a48743823e9e6fde29d20556ed
Artem Kireev <al-mysri@yandex.ru>
2022-06-09 21:37:44 +0300

    fix: add password check

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -11,12 +11,14 @@ const validateUrl = (value) => {
 const validateLogin = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
+    password: Joi.string().required(),
   }),
 });
 
 const validateUser = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
+    password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().custom(validateUrl),
