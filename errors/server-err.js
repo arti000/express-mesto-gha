b1ae1fcafd4080d1057c920b48b6a93f6f8c4968
Artem Kireev <al-mysri@yandex.ru>
2022-06-09 20:57:19 +0300

    fix: delete check from handler

--- errors/server-err.js
+++ errors/server-err.js
@@ -1,17 +1,11 @@
 module.exports = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
-  if (err.kind === 'ObjectId') {
-    res.status(400).send({
-      message: 'Переданы неверные данные',
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
     });
-  } else {
-    res
-      .status(statusCode)
-      .send({
-        message: statusCode === 500
-          ? 'На сервере произошла ошибка'
-          : message,
-      });
-  }
   next();
 };
