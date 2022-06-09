b54a5a68b3c220fb8b1c40ae221a1b15b6900938
Artem Kireev <al-mysri@yandex.ru>
2022-06-09 20:45:03 +0300

    fix: delete password validation props

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -11,14 +11,12 @@ const validateUrl = (value) => {
 const validateLogin = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8).max(30),
   }),
 });
 
 const validateUser = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8).max(30),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().custom(validateUrl),
